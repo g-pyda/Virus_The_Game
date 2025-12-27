56746d57c212e313f3be15eb45dd52bd6a462a1a
catsnravens <wik.ant.s@gmail.com>
2025-12-27 18:46:57 +0100

    changes to player, game exists

--- engine/card.py
+++ engine/card.py
@@ -1,25 +1,27 @@
+from dataclasses import dataclass
+
 colors = ["red", "green", "blue", "yellow", "rainbow"]
 status = ["sick", "vaccinated", "immune", "dead"]
 
 #when a player decides to put out their organ, we initialize a stack for this color -> cant initialize stacks with same color + control whether the card is immune or the organ died
 
 #or maybe card doesnt have to be a class?
+@dataclass
 class Card:
 
-    def __innit__(self, color, value):
-        self.color = color
-        self.value = value
-        #where for value: 1 is vaccine, -1 is virus, 0 is an organ
+    color: str
+    value: int #where for value: 1 is vaccine, -1 is virus, 0 is an organ; do we want it to bt an enum?
 
 class Stack:
     #stack for a card (to add viruses or vaccines)
     # value 2 = immune, -2 = dead, 1 = vaccinated, -1 = sick
-    stack_value = 0
-    status = ""
-    def __innit__(self, Card):
+
+    def __init__(self, Card):
         if(Card.value != 0):
             raise TypeError("Your first card of the color has to be an organ!") 
         else:
+            self.stack_value = 0
+            self.status = "healthy"
             self.color = Card.color
             self.add_card(Card)
 
@@ -33,7 +35,9 @@ class Stack:
         self.set_status()
 
     def set_status(self):
-        match stack_value:
+        match self.stack_value:
+            case 0:
+                self.status = "healthy"
             case 1:
                 self.status = "vaccinated"
             case -1:
--- engine/player.py
+++ engine/player.py
@@ -1,16 +1,16 @@
-class player:
+class Player:
     max_on_hand = 3
-    status = 0 #when status changes to 1 the player wins
 
-
-    def __innit__(self, name):
+    def __init__(self, name):
         self.name = name
         self.on_hand = [] #list of cards on hand
-        self.laid_out = [] #list of cards laid on the table
+        self.laid_out = [] #list of stacks initiated with organ laid on the table
+        self.status = 0 #when status changes to 1 the player wins
+
 
     def move:
         #information to choose what to do
-        action = input("What action do you want to do?")
+        action = input("What action do you want to do?") #later moved to UI
         match action:
 
             case "attack":
@@ -24,8 +24,15 @@ class player:
 
             case: "discard":
             # how many cards you want to discard 
-        self.draw_card(3 - len(on_hand))
 
-    def draw_card:
+        self.draw_card(max_on_hand - len(on_hand)) #draw to always have 3 on hand
+
+    def draw_card(self):
         pass
-        #does this happen in player or game?
+        #does this happen in player or game? -> GAME DRAWS FOR PLAYER
+
+    def add_card_to_stack(self, stack: Stack, card: Card):
+        stack.add_card(card)
+
+        if stack.status == "dead":
+            self.laid_out.remove(stack)
