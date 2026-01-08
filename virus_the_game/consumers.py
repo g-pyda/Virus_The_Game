577335c3bdc07ad0c08f3c46ba7d9c8acfad7741
g-pyda <gabriela.pyda4@gmail.com>
2026-01-08 15:53:18 +0100

    lobby consumer created

--- virus_the_game/consumers.py
+++ virus_the_game/consumers.py
@@ -39,3 +39,42 @@ class GameConsumer(AsyncWebsocketConsumer):
             'type': 'chat',
             'message': message
         }))
+
+
+class LobbyConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print("Connecting to lobby...")
+        self.room_code = self.scope["url_route"]["kwargs"]["room_code"]
+        self.room_group_name = f"{self.room_code}"
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+        print("Host connected to lobby", self.room_group_name)
+        print("Connected to lobby.")
+
+    async def disconnect(self, close_code):
+        print("Disconnected from lobby:", close_code)
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get('message')
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'lobby_message',
+                'message': message
+            }
+        )
+
+    async def lobby_message(self, event):
+        message = event['message']
+
+        await self.send(json.dumps({
+            'type': 'lobby',
+            'message': message
+        }))
